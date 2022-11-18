314d631d697dfc132d5da109e90653a941e96b39
fortu038 <fortu038@umn.edu>
2022-11-17 20:35:24 -0600

    added content to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,20 @@ import React from 'react';
 function Footer () {
   return(
     <div>
-      <p>Testing Footer</p>
+      <a href="https://github.com/fortu038"> 
+        <img 
+          className="footer-image"
+          src={require("../assets/images/GitHub-Mark-64px.png")} 
+          alt="The logo for GitHub. Clicking on it will redirect to my Github account."
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/conor-fortuna-03b995248/"> 
+        <img 
+          className="footer-image"
+          src={require("../assets/images/linkedin-logo.png")} 
+          alt="The logo for LinkedIn. Clicking on it will redirect to my LinkedIn account."
+        />
+      </a>
     </div>
   )
 }
